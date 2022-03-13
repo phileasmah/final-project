c4b1c674c69d1a7ca87cfa6bbd6c19392cec89ce
phileasmah <60140749+phileasmah@users.noreply.github.com>
2022-03-13 20:01:00 +0000

    added loading animation for sign in

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        {session ? (
+        {loading ? (
+          <div className="flex flex-col justify-center align-middle h-screen">
+            <h1 className="text-center text-5xl font-medium">Music Taste Explorer</h1>
+            <div className="mx-auto mt-4">
+              <div className="flex px-2.5 md:px-4 py-2 font-medium bg-spotify1 rounded text-darkgrey">
+                <svg
+                  className="animate-spin w-4 h-4 rounded-full bg-transparent border-2 border-transparent mr-2 mt-0.5"
+                  style={{ borderRightColor: "#E1E3FB", borderTopColor: "#E1E3FB" }}
+                ></svg>
+                <b className="text-darkgrey">Loading...</b>
+              </div>
+            </div>
+            <h2 className="text-center mt-4">
+              All connections are secure and no user data will be stored remotely
+            </h2>
+          </div>
+        ) : session ? (
           <div>
             <UserPlaylists session={session} />
           </div>
@@ -27,7 +43,9 @@ export default function Home() {
                 Sign in with <b className="text-darkgrey">Spotify</b>
               </button>
             </div>
-            <h2 className="text-center mt-4">All connections are secure and no user data will be stored remotely</h2>
+            <h2 className="text-center mt-4">
+              All connections are secure and no user data will be stored remotely
+            </h2>
           </div>
         )}
       </div>
